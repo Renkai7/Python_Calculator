37ac5064a50e2a5303a71e1db4966e1f203f5b7c
Michael Spencer <ms53150@gmail.com>
2022-03-19 23:02:44 +0000

    Add recursion with calculator function. Allow user to keep calculating

--- main.py
+++ main.py
@@ -24,16 +24,29 @@ operations = {
   "/": divide
 }
 
-num1 = int(input("What's the first number?: "))
-
-for symbols in operations:
-  print(symbols)
-operation_symbol = input("Pick an operation from the line above: ")
-
-num2 = int(input("What's the second number?: "))
-
-# assign chosen function from dictionary to answer
-calculations = operations[operation_symbol]
-answer = calculations(num1, num2)
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+# Recursion
+def calculator():
+  num1 = int(input("What's the first number?: "))
+  # list operation symbols
+  for symbols in operations:
+    print(symbols)
+    
+  continue_calculation = True
+  while continue_calculation:
+    operation_symbol = input("Pick an operation from the line above: ")
+    num2 = int(input("What's the next number?: "))
+    # assign chosen function from dictionary to answer
+    calculations = operations[operation_symbol]
+    answer = calculations(num1, num2)
+  
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+  
+    if input(f"Type 'y' to continue calculating {answer}, or type 'n' to start a new calculation: ").lower() == 'y':
+      num1 = answer
+    else:
+      continue_calculation = False
+      calculator()
+          
+calculator()
+    
+  
